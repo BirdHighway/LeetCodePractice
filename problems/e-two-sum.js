688d614f2dc5a099b89cddaba181cbcb66fc2cc5
Stephen Hyde <shh2112@gmail.com>
2021-08-15 10:57:36 -0400

    Initial commit

---
+++ problems/e-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+  for (let a = 0; a < nums.length - 1; a++) {
+      for (let b = a + 1; b < nums.length; b++) {
+          if (nums[a] + nums[b] === target) {
+              return [a, b];
+          }
+      }
+  }
+};
+
+module.exports = twoSum;
---
+++ problems/e-two-sum.test.js
@@ -0,0 +1,13 @@
+const twoSum = require('./e-two-sum.js');
+
+test('finds indices of nums that add up to target', () => {
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+  expect(twoSum(nums, target)).toEqual([0, 1]);
+});
+
+test('finds correct indices when nums identical', () => {
+  const nums = [3, 3];
+  const target = 6;
+  expect(twoSum(nums, target)).toEqual([0, 1]);
+});

1d2bac1060612e12710449e0e8a043869b8a7e86
Stephen Hyde <shh2112@gmail.com>
2021-08-15 11:36:03 -0400

    Medium: string to integer

--- problems/m-atoi.js
+++ problems/m-atoi.js
@@ -3,5 +3,29 @@
  * @return {number}
  */
  var myAtoi = function(s) {
-
+  const MAX_INT = Math.pow(2, 31) - 1;
+  const MIN_INT = -Math.pow(2, 31);
+  let c, sign = null, digits = '', i = 0;
+  while (c = s.charAt(i++)) {
+    if (sign === null) {
+      if (c === ' ') continue;
+      if (c === '-' || c === '+') {
+        sign = c === '-' ? -1 : 1;
+        continue;
+      }
+    }
+    if (c > '/' && c < ':') {
+      sign = sign === null ? 1 : sign;
+      digits += c;
+      continue;
+    }
+    break;
+  }
+  if (digits === '') return 0;
+  result = sign * digits;
+  if (result > MAX_INT) return MAX_INT;
+  if (result < MIN_INT) return MIN_INT;
+  return result;
 };
+
+module.exports = myAtoi;
---
+++ problems/m-atoi.test.js
@@ -0,0 +1,47 @@
+const myAtoi = require('./m-atoi.js');
+
+test('works when all characters are digits', () => {
+  const s = '42';
+  expect(myAtoi(s)).toBe(42);
+});
+
+test('works on negative numbers', () => {
+  const s = '-42';
+  expect(myAtoi(s)).toBe(-42);
+});
+
+test('rejects multiple signs', () => {
+  const s1 = '-+12';
+  expect(myAtoi(s1)).toBe(0);
+  const s2 = '+-8';
+  expect(myAtoi(s2)).toBe(0);
+});
+
+test('rejects repeated signs', () => {
+  const s1 = '--12';
+  expect(myAtoi(s1)).toBe(0);
+  const s2 = '++12';
+  expect(myAtoi(s2)).toBe(0);
+});
+
+
+test('works with words appended', () => {
+  const s = '4193 with words';
+  expect(myAtoi(s)).toBe(4193);
+});
+
+test('returns 0 when started with words', () => {
+  const s = 'words and 987';
+  expect(myAtoi(s)).toBe(0);
+});
+
+test('works with leading whitespace', () => {
+  const s = '    1987';
+  expect(myAtoi(s)).toBe(1987);
+});
+
+test('clamps overflowing ints', () => {
+  const s = '-91283472332';
+  expect(myAtoi(s)).toBe(-2147483648);
+});
+
